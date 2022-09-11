14d95885cc7547d5ba5c30193411576d137b9cec
Sarah <sarah.vujcich.school@gmail.com>
2022-09-11 13:20:42 -0700

    updating visual asthetics of map

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -25,8 +25,23 @@ let map = L.map('mapid').setView([40.7, -94.5], 4);
 // Add the 'graymap' tile layer to the map.
 // streets.addTo(map);
 
+// 4a. Add a marker to the map for Los Angeles, California.
+// let marker = L.marker([34.0522, -118.2437]).addTo(map);
+
+//4b. change marker to a circle -- ALTERNATIVE: L.circleMarker([34.0522, -118.2437]).addTo(map);
+//  L.circle([34.0522, -118.2437], {
+//     radius: 100
+//  }).addTo(map);
+
+//4c. make the marker pretty
+ L.circle([34.0522, -118.2437], {
+    radius: 300,
+    color: "black",
+    fillColor: '#ffffa1'
+ }).addTo(map);
+
 // 3b refactor 3a
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
